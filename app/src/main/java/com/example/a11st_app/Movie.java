b0971f3816ad2d508bd1dd405c9102954521759f
wjdduslee <qpalzm020830@naver.com>
2022-05-24 16:15:04 +0900

    영화정보의수 추가

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,21 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndnew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAnt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
