779ab15280fe4b5b2587dba3c0dec4645fc1cdb5
wjdduslee <qpalzm020830@naver.com>
2022-06-11 14:01:37 +0900

    movie_item에 rank,audiAcc,openDt 추가 함 기말시험 21173072 이정연 제출

---
+++ app/src/main/java/com/example/a11st_app/MovieAdapter.java
@@ -0,0 +1,70 @@
+package com.example.a11st_app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    ArrayList<Movie> items = new ArrayList<Movie>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.movie_item,viewGroup,false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Movie item = items.get(position);
+        viewHolder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    public void addItem (Movie item){
+        items.add(item);
+    }
+    public void setItems(ArrayList<Movie> items){
+        this.items =items;
+    }
+    public Movie getItem(int position){
+        return items.get(position);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView movieName;
+        TextView peopleCount;
+        TextView rank;
+        TextView audiAcc;
+        TextView openDt;
+
+                public ViewHolder(View itemView){
+                    super(itemView);
+                    movieName = itemView.findViewById(R.id.movieName);
+                    peopleCount = itemView.findViewById(R.id.peopleCount);
+                    rank = itemView.findViewById(R.id.rank);
+                    audiAcc = itemView.findViewById(R.id.audiAcc);
+                    openDt = itemView.findViewById(R.id.openDt);
+                }
+                public void setItem(Movie item){
+                    movieName.setText(item.movieNm);
+                    peopleCount.setText(item.audiCnt);
+                    rank.setText(item.rank);
+                    audiAcc.setText(item.audiCnt);
+                    openDt.setText(item.openDt);
+
+                }
+
+    }
+}
